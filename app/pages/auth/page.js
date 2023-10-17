b5489e26287075d123279543e851dd440e82d54a
builtbydans <danish.shafi@live.co.uk>
2023-10-17 11:29:41 +0100

    Changes to settings

---
+++ app/pages/auth/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const Auth = () => {
+  return (
+    <>
+      <div>This is the Auth page</div>
+      <p>
+        <Link href="/">Back to Home</Link>
+      </p>
+    </>
+  );
+};
+
+export default Auth;
